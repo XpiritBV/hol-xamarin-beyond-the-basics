4b6c0da1e8974ccfade9ae8c296dae46505dd0c2
Roy Cornelissen <roy_cornelissen@hotmail.com>
2019-11-14 09:21:00 +0100

    💄 Add Dark Mode

--- app/src/ConferenceApp.Android/MainActivity.cs
+++ app/src/ConferenceApp.Android/MainActivity.cs
@@ -1,12 +1,15 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Content.Res;
 using Android.OS;
 using Android.Runtime;
+using ConferenceApp.Services;
+using ConferenceApp.Styles;
 using Shiny;
 
 namespace ConferenceApp.Droid
 {
-    [Activity(Label = "ConferenceApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "ConferenceApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -24,6 +27,8 @@ namespace ConferenceApp.Droid
             global::Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
 
             LoadApplication(new App());
+
+            ThemeService.ApplyTheme(GetOSTheme(Resources.Configuration));
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
@@ -33,6 +38,33 @@ namespace ConferenceApp.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public override void OnConfigurationChanged(Configuration newConfig)
+        {
+            base.OnConfigurationChanged(newConfig);
+
+            ThemeService.ApplyTheme(GetOSTheme(newConfig));
+        }
+
+        public string GetOSTheme(Configuration configuration)
+        {
+            //Ensure the device is running Android Froyo or higher because UIMode was added in Android Froyo, API 8.0
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Froyo)
+            {
+                var uiModeFlags = configuration.UiMode & UiMode.NightMask;
+
+                switch (uiModeFlags)
+                {
+                    case UiMode.NightYes:
+                        return nameof(DarkTheme);
+                    case UiMode.NightNo:
+                        return nameof(LightTheme);
+                }
+            }
+
+            // default theme
+            return nameof(LightTheme);
         }
     }
 }
